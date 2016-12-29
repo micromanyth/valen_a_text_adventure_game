14a962ad92d48c0527601cf81f9ca429ba01a5a0
micromanyth <miromanyth@gmail.com>
2016-12-29 15:22:57 -0500

    Add files via upload

---
+++ playerClass.py
@@ -0,0 +1,10 @@
+class player():
+	def __init__(self, baseHealth, baseAttack, baseGold, startWeap):
+		self.baseHealth = 100
+		self.baseAttack = 5
+		self.baseGold = 125
+		self.startWeap = "Old Battered Sword"
+
+#class weapons():
+#	def __init__(attack, description)
+#		self.
